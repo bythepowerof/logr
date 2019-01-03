074c2ca1fe44f507823d8209709e296280e58924
Bryan T. Richardson <bryan@activeshadow.com>
2019-01-03 05:58:56 -0700

    [v0.1.0] Release
    * Refactored utils --> util
    * Go module support

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/activeshadow/logr
+
+require github.com/go-logr/logr v0.1.0
--- stdlogr/logger.go
+++ stdlogr/logger.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/activeshadow/logr/utils"
+	"github.com/activeshadow/logr/util"
 	"github.com/go-logr/logr"
 )
 
